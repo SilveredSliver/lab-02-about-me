eeb5cb25fe0f5b7ecd66817c56f1a99aa8c590d3
AaronBruce <aaron.bruce@comcast.net>
2017-04-04 21:59:03 -0700

    This should be the last entry before I submit this assignment the first time.

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ lab-02-about-me
 
 I used variables and if/else if statements to determine proper responses to user input values in the prompts that the code initiates.
 
-blah blah blah
+This assignment took me a while, a long while.
+I was unsure which documents required what info at first and I had a tough time remembering exactly what steps I needed to do in order to push this local git repo into the cloud repo (I am still unclear on terminology at the moment).
+  I created the first template for the first question in the app.js file and then copied templates but with slightly different variations to see if they would still work and to keep my memory of the different ways to make this code work expanded.
+Overall, at the end of the day, I still feel a little intimidated by the whole assignment when starting from scratch, but I find as I progress further and further through each project, I am able to become more confident in my abilities to code and retain the information I dish out.
--- app.js
+++ app.js
@@ -3,15 +3,15 @@
     console.log('user input values to prompts are listed below');
 
 
-    var sushi = prompt('Do you like sushi?');
-      if (sushi.trim().toLowerCase() === 'yes') {
-        alert('You like sushi!');
-      } else if (sushi.trim().toLowerCase() === 'no'){
-        alert('You are a bum!');
+    var aliens = prompt('Do you like aliens?');
+      if (aliens.trim().toLowerCase() === 'yes') {
+        alert('You weirdo!');
+      } else if (aliens.trim().toLowerCase() === 'no') {
+        alert("You are heartless!");
       } else {
         alert('You failed to input a proper answer!')
       }
-      console.log('user input to prompt sushi was ' + sushi);
+      console.log('user input to prompt aliens was ' + aliens);
 
 
     var banana = prompt('Are you a banana?');
@@ -26,7 +26,7 @@
 
     var turtle = prompt('Are you a turtle?');
       if (turtle.trim().toLowerCase() === 'yes') {
-        var clarify = confirm('Are you sure?');
+    var clarify = confirm('Are you sure?');
         if (clarify === true) {
           alert('You are a weird turtle!');
         } else {
@@ -39,3 +39,32 @@
       }
       console.log('user input to prompt turtle was ' + turtle);
       console.log('user input to prompt clarify was ' + clarify);
+
+    var name = prompt('Is your name Weirdo Turtle Alien?');
+      if (name.trim().toLowerCase() === 'yes') {
+        alert("Bwahahaha!!! That's hilarious!");
+      } else if (name.trim().toLowerCase() === 'no') {
+    var clarifyAgain = prompt('Are you sure that your name is Weirdo Turtle Alien?');
+      if (clarifyAgain.trim().toLowerCase() === 'no') {
+        alert("There is nothing you can do to change the name I've given you!")
+      } else {
+        alert('I knew it!');
+      }
+      console.log('user input to prompt name was ' + name);
+      console.log('user input to prompt clarifyAgain was ' + clarifyAgain);
+      }
+    var feedback = prompt('Did you like this short but invasive personal test?');
+      if (feedback.trim().toLowerCase() === 'yes') {
+    var clarity = confirm('Are you sure?');
+              if (clarify === true) {
+                alert('You are amazing! ...Mostly!');
+              } else {
+                alert("No takebacks!");
+              }
+            } else if (turtle.trim().toLowerCase() === 'no') {
+              alert ('You know you actually liked it! :3');
+            } else {
+              alert("These are simple yes or no questions! Get it right!")
+            }
+      console.log('user input to prompt feedback was ' + feedback);
+      console.log('user input to confirm clarity was ' + clarity);
